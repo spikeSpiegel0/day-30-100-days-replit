824f3db1ee316a1c466a392756a43e906922137f
rookie <121451865+spikeSpiegel0@users.noreply.github.com>
2023-01-08 10:01:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "Katie"
+age = "28"
+pronouns = "she/her"
+
+response = f"""This is {name}, using {pronouns} pronouns, and is {age} years old. Hello, {name}. 
+
+How are you? Have you been having a great {age} years so far"""
+
+print(response)
